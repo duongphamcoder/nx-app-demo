4a978f8d383429c8c5dfea2365c07d207d570432
duong.pham <phamtanduongtk29@gmail.com>
2023-09-01 01:21:59 +0700

    add lib

--- apps/react-app/src/app/nx-welcome.tsx
+++ apps/react-app/src/app/nx-welcome.tsx
@@ -5,8 +5,16 @@
  Delete this file and get started with your project!
  * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
+
+import { validates } from '@nx-app-demo/validates';
+
 export function NxWelcome({ title }: { title: string }) {
-  return <h1>Hello Duong Pham</h1>;
+  return (
+    <>
+      <h1>Hello Duong Pham</h1>
+      <p>Currently, you use one lib is {validates()}</p>
+    </>
+  );
 }
 
 export default NxWelcome;
